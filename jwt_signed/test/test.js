cc09aa3ce21770473e7d4d1b341403c1d0707bcb
mukundha@apigee.com <mukundha@apigee.com>
2016-08-30 17:06:11 -0700

    fix cors and test

--- jwt_signed/test/test.js
+++ jwt_signed/test/test.js
@@ -1,6 +1,4 @@
-var assert = require('assert');
-var request = require('request');
-
+var assert = chai.assert;
 var pathname = window.location.pathname
 var splits = pathname.split('/')
 //expected /v1/o/:orgname/e/:env/samples/:sample/test.html
@@ -12,42 +10,50 @@ getTestData(org,env,sample,function(data){
   //i don have any data for this proxy
 })
 
-var create_url = 'http://' + org + '-' + env + '.apigee.net/jwt_signed/create-hs256'
-var verify_url = 'http://' + org + '-' + env + '.apigee.net/jwt_signed/validate-hs256'
+var create_url = 'https://' + org + '-' + env + '.apigee.net/jwt_signed/create-hs256'
+var verify_url = 'https://' + org + '-' + env + '.apigee.net/jwt_signed/validate-hs256'
 
 describe('Running tests for JWT', function() {
   var jwt=''
     it('get a jwt token', function(done) {
-      request({
-      	url: create_url,
-      	method:'POST',
-      	body:'key=12345678912345678912345678912345',
-        headers: {
+
+      $.ajax({
+          url:create_url,
+          method: 'POST',
+          data : 'key=12345678912345678912345678912345',
+          headers: {
           'Content-Type':'application/x-www-form-urlencoded'
-        }
-      } , function(err, response,body){
-           console.log(body)
-           jwt=JSON.parse(body).jwt
-      	   assert.equal(response.statusCode,200)
-           assert(body)
-           done()
+          },
+          success:function(body){ 
+            console.log(body)
+            jwt=body.jwt
+            assert(body)
+            done()
+          },
+          error: function(xhr,status,error){
+            done(error)
+          }
       })
     })
+      
 
     it('verify the generated jwt', function(done){
-      request({
-        url: verify_url,
-        method:'POST',
-        body:'key=12345678912345678912345678912345&jwt='+jwt,
-        headers: {
+      $.ajax({
+          url:verify_url,
+          method: 'POST',
+          data : 'key=12345678912345678912345678912345&jwt=' + jwt,
+          headers: {
           'Content-Type':'application/x-www-form-urlencoded'
-        }
-      } , function(err, response,body){
-           console.log(body)
-           assert.equal(response.statusCode,200)
-           assert(body)
-           done()
+          },
+          success:function(body){ 
+            console.log(body)            
+            done()
+          },
+          error: function(xhr,status,error){
+            done(error)
+          }
       })
     })
 })
+
 
